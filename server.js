f9ca2d55706b22cc03878180d270a849c5720395
amitkas <amit.kashtan@gmail.com>
2017-07-05 10:17:11 +0300

    setup

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/angularbeers', function () {
+  console.log("DB connection established!!!");
+})
+
 
 var app = express();
 
